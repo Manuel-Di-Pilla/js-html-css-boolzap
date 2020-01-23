df427ca3af862fb233ad8683889e0c9c92f2a1cb
Manuel <manueldipilla3@gmail.com>
2020-01-23 20:54:38 +0100

    milestone 1 completo ma con un bug

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
+$(document).ready(
+  function () {
+    $('.send').click(
+      function () {
+        var text = $('.add-message').val();
+        var newMessage = $('.template').clone();
+        newMessage.addClass('text').addClass('me');
+        var find = newMessage.find('p');
+        find.text(text);
+        $('.live-chat').append(newMessage);
+      }
+    )
+  }
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $('.list li').click(
   function () {
     if ($(this).hasClass('click-background') == false) {
